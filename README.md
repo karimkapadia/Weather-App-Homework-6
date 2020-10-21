1edaec51f74067efa6426c8af8973a0d7224d2a9
Karimk <karimkapadia@gmail.com>
2020-10-20 18:14:20 -0600

    update UI in index.html and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Weather-App-Homework-6
-This is a weather app.
+This is a weather application which helps user to get weather forecast for today and 5 days in future.  In current day's weather it displays date, tempreture, humidity and wind speed. In 5 day future weather it displays date, tempreture and humidity. When a user search for city then it will give current day's weather and 5 day forecast and city will be added to search histoy. Then if user click on city in search history then again current and 5 day weather forcast will be present.
+
+# API used.
+[OpenWeather API](https://openweathermap.org/api)
+
+
+# Images
+![Home page](./images/homepage.png)
+![Search City page](./images/searchCitypage.png)
+![HSearch History page](./images/localstoragepage.png)
+
+# Git Hub
+Repo Link : https://github.com/karimkapadia/Weather-App-Homework-6
+Live Link : 
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var apiKey ="78b8cd49761ceab314734264668b8b2d";
-
+var resp= ""
 var searchCity = "";
+var citySaved = [];
+
  showResult();
 
+//  $("#title").html("Hello")
 
-
-function showResult(city)
+ async function showResult(city)
  {
      if(city)
      {
@@ -16,23 +18,76 @@ function showResult(city)
         searchCity="calgary";
         
     }
-    // console.log(searchCity);
+    console.log(searchCity);
+    var currqueryURL ="http://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=metric&appid="+apiKey;
+    await fetch(currqueryURL)
+    .then(response1 => response1.text())
+    .then(str => resp1=(JSON.parse(str)))
+    console.log(resp1);
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ searchCity+"&units=metric&appid="+apiKey;
+     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ searchCity+"&units=metric&appid="+apiKey;
     console.log(queryURL);
 
-    $.ajax({
-        URL: queryURL,
-        method: "GET",
-    }).then(function(response)
-        {
-        // console.log(queryURL)
-        
-        console.log(response);
-    });
+
+ await fetch(queryURL)
+  .then(response => response.text())
+  .then(str => resp2=(JSON.parse(str)))
+   console.log(resp2);
+
+
+ var date = moment.unix(resp1.dt);
+//   currDate=parseInt(resp.list[0].dt_txt.split(" ")[0].split("-")[2]-1);
+  var y= date._d.constructor().split(" ");
+  console.log(y[3],y[2],y[1]);
+
+document.querySelector("#mainTitle").innerHTML  = searchCity +" : "+y[3]+"/"+y[2]+"/"+y[1];
+document.querySelector("#mainTemp").innerHTML = "Tempreture :"+ resp1.main.temp;
+document.querySelector("#mainHumidity").innerHTML = "Humidity :" +resp1.main.humidity;
+document.querySelector("#mainWind").innerHTML = "Wind : "+ resp1.wind.speed;
+
+document.querySelector("#day1Date").innerHTML =  resp2.list[0].dt_txt.split(" ")[0];
+document.querySelector("#day1Temp").innerHTML = "Tempreture :" + Math.round(resp2.list[0].main.temp);
+document.querySelector("#day1Humidity").innerHTML = "Wind : "+ resp2.list[0].main.humidity;
+
+document.querySelector("#day2Date").innerHTML =  resp2.list[8].dt_txt.split(" ")[0];
+document.querySelector("#day2Temp").innerHTML = "Tempreture :" + Math.round(resp2.list[8].main.temp);
+document.querySelector("#day2Humidity").innerHTML = "Wind : "+ resp2.list[8].main.humidity;
+
+document.querySelector("#day3Date").innerHTML =  resp2.list[16].dt_txt.split(" ")[0];
+document.querySelector("#day3Temp").innerHTML = "Tempreture :" + Math.round(resp2.list[16].main.temp);
+document.querySelector("#day3Humidity").innerHTML = "Wind : "+ resp2.list[16].main.humidity;
+
+document.querySelector("#day4Date").innerHTML =  resp2.list[24].dt_txt.split(" ")[0];
+document.querySelector("#day4Temp").innerHTML = "Tempreture :" + Math.round(resp2.list[24].main.temp);
+document.querySelector("#day4Humidity").innerHTML = "Wind : "+ resp2.list[24].main.humidity;
+
+document.querySelector("#day5Date").innerHTML =  resp2.list[32].dt_txt.split(" ")[0];
+document.querySelector("#day5Temp").innerHTML = "Tempreture :" + Math.round(resp2.list[32].main.temp);
+document.querySelector("#day5Humidity").innerHTML = "Humidity : "+ resp2.list[32].main.humidity;
+    
+   
+    
 }
 
- 
+function showSearch(event)
+{
+    event.preventDefault()
 
- 
+    // console.log(document.querySelector("#serachText").value);
+    searchCity= document.querySelector("#serachText").value;
+    showResult(searchCity);
+    // var listDiv = document.createElement("div")
+    var button =document.createElement("button");
+    button.textContent = searchCity;
+    // button.addEventListener("onclick");
+    // button.onclick(showResult(button.textContent));
+    document.querySelector("#cityList").prepend(button);
+    citySaved.push(searchCity);
+    localStorage.setItem("cityList",citySaved );
 
+
+    // listitem.innerHTML =searchCity;
+    // document.querySelector("#cityList").appendChild(listitem);
+
+
+}
